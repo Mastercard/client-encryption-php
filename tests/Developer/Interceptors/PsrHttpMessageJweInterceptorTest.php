998f8e998118e658b7c7b81c939b921f4bb559bc
talha-API <123556329+talha-API@users.noreply.github.com>
2023-03-14 16:36:43 +0000

    Removing exception for A192GCM not being supported

--- tests/Developer/Interceptors/PsrHttpMessageJweInterceptorTest.php
+++ tests/Developer/Interceptors/PsrHttpMessageJweInterceptorTest.php
@@ -142,30 +142,6 @@ class PsrHttpMessageJweInterceptorTest extends TestCase
         $this->assertEquals(0, sizeof($response->getHeaders()));
     }
 
-    public function testInterceptResponse_ShouldThrowAnExceptionWhenEncryptionNotSupported()
-    {
-        $this->expectException(EncryptionException::class);
-        $this->expectExceptionMessage('Encryption method A192GCM not supported');
-
-        // GIVEN
-        $encryptedPayload = "{" .
-            "\"encryptedPayload\":\"eyJraWQiOiI3NjFiMDAzYzFlYWRlM2E1NDkwZTUwMDBkMzc4ODdiYWE1ZTZlYzBlMjI2YzA3NzA2ZTU5OTQ1MWZjMDMyYTc5IiwiY3R5IjoiYXBwbGljYXRpb25cL2pzb24iLCJlbmMiOiJBMTkyR0NNIiwiYWxnIjoiUlNBLU9BRVAtMjU2In0.peSgTt_lPbcNStWh-gI3yMzhOGtFCwExFwLxKeHwjzsXvHB0Fml5XnG0jRbJSfOHzKx02d0NVBzoDDRSAnafuabbbMKcoaUK-jZNHSg4BHdyBZpCO82kzvWeEm3TTNHIMBTfM00EmdFB03z_a0PaWsT-FIOzu4Sd5Z_nsNLhP9941CtVS-YtZ9WkgDezGipxA7ejQ3X5gFVy2RH1gL8OTbzIYCwBcrfSjAiCQgunNbLxPPlfZHB_6prPK7_50NS6FvuMnAhiqUiiAka8DHMdeGBWOie2Q0FV_bsRDHx_6CY8kQA3F_NXz1dELIclJhdZFfRt1y-TEfwOIj4nDi2JnA.8BYMB5MkH2ZNyFGS._xb3uDsUQcPT5fQyZw.O0MzJ5OvNyj_QMuqaloTWA\"}";
-
-        $decryptionKey = DecryptionKey::load("./resources/Keys/Pkcs8/test_key_pkcs8-2048.pem");
-
-        $config = JweConfigBuilder::aJweEncryptionConfig()
-            ->withDecryptionKey($decryptionKey)
-            ->withDecryptionPath("$.encryptedPayload", "$.foo")
-            ->build();
-
-        $headers = ['Content-Type' => 'application/json'];
-        $response = new Response(200, $headers, $encryptedPayload);
-
-        // WHEN
-        $instanceUnderTest = new PsrHttpMessageJweInterceptor($config);
-        $instanceUnderTest->interceptResponse($response);
-    }
-
     public function testInterceptResponse_ShouldThrowEncryptionException_WhenDecryptionFails()
     {
         $this->expectException(EncryptionException::class);
